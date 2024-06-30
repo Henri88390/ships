d33b308a8cade29a3a94349da7c2b69128c317c5
Henri88390 <henri.lambert9@gmail.com>
2024-06-30 18:29:45 +0200

    delete app test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Ships", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Ships/i);
-  expect(linkElement).toBeInTheDocument();
-});
